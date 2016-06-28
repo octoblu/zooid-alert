540bdac7383f688d657cfcbc24e87d8248162900
Sqrt of Saturn <sqrt@octoblu.com>
2016-06-27 19:07:40 -0700

    v1.0.0 Release

--- src/Alert/index.js
+++ src/Alert/index.js
@@ -1,10 +1,58 @@
+import isEmpty from 'lodash.isempty'
 import React, { PropTypes } from 'react'
+import classNames from 'classnames'
 
-const propTypes = {}
-const defaultProps = {}
+import DismissButton from '../DismissButton/'
+import styles from './styles.css'
 
-const Alert = () => {
-  return <div>Alert</div>
+const ALERT_TYPES = [
+  'info',
+  'warning',
+  'error',
+  'success',
+]
+
+const propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  dismissible: PropTypes.bool,
+  type: PropTypes.oneOf(ALERT_TYPES),
+}
+
+const defaultProps = {
+  type: ALERT_TYPES[0],
+}
+
+class Alert extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { dismissed: false }
+    this.handleDismiss = this.handleDismiss.bind(this)
+  }
+
+  handleDismiss() {
+    this.setState({ dismissed: true })
+  }
+
+  render() {
+    const { children, className, dismissible, type } = this.props
+
+    if (isEmpty(children)) return null
+    if (this.state.dismissed) return null
+
+    const classes = classNames(
+      { [`${styles[type]}`]: type },
+      className
+    )
+
+    return (
+      <div className={classes}>
+        {children}
+        {dismissible && <DismissButton onDismiss={this.handleDismiss} />}
+      </div>
+    )
+  }
 }
 
 Alert.propTypes    = propTypes
--- stories/index.js
+++ stories/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { storiesOf, action } from '@kadira/storybook'
+import { storiesOf } from '@kadira/storybook'
 
 import Alert from '../src'
 
 storiesOf('Alert', module)
-  .addWithInfo('Basic', 'added Description', () => (
-    <Alert />
+  .addWithInfo('Basic', 'Alerts for days!', () => (
+    <div>
+      <Alert>Basic Info Message</Alert>
+      <Alert type="warning">Basic Warning Message</Alert>
+      <Alert type="error">Basic Error Message</Alert>
+      <Alert type="success">Basic Success Message</Alert>
+    </div>
   ), { inline: true })
-  .add('Basic', () => (
-    <Alert />
-  ))
+  .addWithInfo('Dismissible', 'A dismissible alert', () => (
+    <div>
+      <Alert dismissible>Dismissible Alert Message</Alert>
+      <Alert type="warning" dismissible>Dismissible Warning Message</Alert>
+      <Alert type="error" dismissible>Dismissible Error Message</Alert>
+      <Alert type="success" dismissible>Dismissible Success Message</Alert>
+    </div>
+  ), { inline: true })
